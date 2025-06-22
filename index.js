bb4874252a64027a5456096b93062fba4b23c3e2
Михаил <st.mihaile87@gmail.com>
2025-06-22 07:45:00 +0400

    Dom_1

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+1
+
+const headingEl = document.getElementById('heading');
+console.log(headingEl);
+
+const titleEl = document.getElementsByTagName('h1');
+console.log(titleEl);
+
+const textEl = document.getElementsByClassName('text');
+console.log(textEl);
+for (let i = 0; i < textEl.length; i++) {
+    const element = textEl[i];
+    console.log(element);
+    
+}
+
+const headingQs = document.querySelector('#heading');
+console.log(headingQs);
+
+const titleQs = document.querySelector('h1');
+console.log(titleQs);
+
+const textQs = document.querySelectorAll('p');
+console.log(textQs);
+
+textQs.forEach(element => {
+    console.log(element);
+});
+
+*/
